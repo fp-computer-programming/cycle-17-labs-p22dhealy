e263f21b8d565d51d9363aafbcbe1b5a052cc212
p22dhealy <p22dhealy@fairfieldprep.org>
2022-04-11 10:18:31 -0400

    Submit lab 17

---
+++ lab_17-1.py
@@ -0,0 +1,21 @@
+#author DMH 4/11/22
+
+import math
+
+
+class Circle:
+    """ Circle class represents a circle """
+
+    def __init__(self):
+        """ Create a new circle with radius 3 """
+
+        self.radius = 3
+
+    def get_area(self):
+        """ Calculate area of circle """
+
+        return self.radius ** 2 * math.pi
+
+my_circle = Circle()
+
+print(my_circle.get_area())
